e4fa894bc228e29e0fa93dd9f7991ed8ab080ae9
David Waza <wazabanga00@gmail.com>
2023-05-20 09:07:04 +0100

    re-modified the gallery page

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -16,25 +16,115 @@ export default function Gallery() {
       <Container>
         <Nav />
         <div className={biostyles.header}>
-          <p className="mb-5">Gallery</p>
+          <p>Gallery</p>
         </div>
-        <Row>
-          {Slides.map(({ src }, index) => (
-            <Col xs={4} sm={4} md={6} key={index}>
-              <img
-                src={src}
-                alt="alt"
-                onClick={() => setOpen(true)}
-                className={styles.imgSize}
-              />
-            </Col>
-          ))}
-        </Row>
-        <Lightbox
-          open={open}
-          close={() => setOpen(false)}
-          slides={Slides}
-        ></Lightbox>
+        <section className="text-gray-400">
+          <div className="container px-5 py-4 mx-auto flex flex-wrap">
+            <div className="flex flex-wrap md:-m-2 -m-1">
+              <div className="flex flex-wrap w-full md:w-1/2">
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img1.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img2.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full">
+                  <video controls>
+                    <source src='vid1.mp4' type='video/mp4' />
+                  </video>
+                </div>
+              </div>
+              <div className="flex flex-wrap w-full md:w-1/2">
+                <div className="md:p-2 p-1 w-full">
+                <video controls>
+                    <source src='vid2.mp4' type='video/mp4' />
+                  </video>
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img3.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img4.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap w-full md:w-1/2">
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img5.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/img6.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full">
+                  <img
+                    alt="gallery"
+                    className="w-full h-[400px] object-cover object-center block"
+                    src="/img7.jpg"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap w-full md:w-1/2">
+                <div className="md:p-2 p-1 w-full">
+                  <img
+                    alt="gallery"
+                    className="w-full h-[400px] object-cover object-center block"
+                    src="/img8.jpg"
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/biohero.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+                <div className="md:p-2 p-1 w-full md:w-1/2">
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-[400px] object-center block"
+                    src="/waterman.jpg"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <Lightbox
+            open={open}
+            close={() => setOpen(false)}
+            slides={Slides}
+          ></Lightbox>
+        </section>
       </Container>
     </section>
   );
