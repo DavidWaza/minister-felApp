a55cc4df3f024d08b0aa8c031458a6aa5648b223
David Waza <wazabanga00@gmail.com>
2023-05-20 09:14:42 +0100

    added a navbar to tribute page

--- src/app/tribute/page.jsx
+++ src/app/tribute/page.jsx
@@ -1,17 +1,14 @@
 "use client";
 import Form from "../components/Form";
+import Nav from "../components/Nav";
 import CommentBlock from "../components/commentBlock";
 
 export default function Tribute() {
   return (
     <div>
-      <div className="bg-[#444654]">
-        {/* <CommentBlock
-          src="/img1.jpg"
-          alt="alt"
-          name="david waza"
-          comments="i love food"
-        /> */}
+      <Nav />
+      <div className="pt-[7%] bg-[#444654]">
+      
       </div>
       <Form />
     </div>
